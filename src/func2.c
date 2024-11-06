7a9637fa157764f7aaf2a55e24f766fbcaa514b9
Kang, Hyeon <hyeony10316@gmail.com>
2024-11-06 13:54:59 +0900

    func2.c func2.h are added

---
+++ src/func2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "func2.h"
+
+void goo(){
+    printf("goo\n");
+}
--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include "func1.h"
+#include "func2.h"
 
 int main(void){
     foo();
+    goo();
     printf("this is main\n");
     return 0;
 }
